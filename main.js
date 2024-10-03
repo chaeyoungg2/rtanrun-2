10ef5a9038e0d22df296dfa14b9ef87c20d9b09b
chaeyoungg2 <rcy040730@naver.com>
2024-10-03 17:56:01 +0900

    Update main.js

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ defeatSound.src = "./sounds/defeat1.mp3";
 /** 이미지 객체 생성 및 설정 */
 // (1) 배경
 const bgImage = new Image();
-bgImage.src = "./images/background.png";
+bgImage.src = "./images/image_fx_.jpg";
 // (2) 게임 시작
 const startImage = new Image();
 startImage.src = "./images/gamestart.png";
@@ -51,7 +51,7 @@ const rtanCrashImage = new Image();
 rtanCrashImage.src = "./images/rtan_crash.png";
 // (8) 장애물
 const obstacleImage = new Image();
-obstacleImage.src = "./images/obstacle1.png";
+obstacleImage.src = "./images/obstacle3.jpg";
 
 /** 1-1 르탄이 그리기 */
 const RTAN_WIDTH = 100; // 르탄이 가로 너비
